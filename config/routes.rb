77d853d2cee2c2c2867fd2407a40a0a402d7266d
Andres delaTorre <andresdevlatorre@gmail.com>
2024-06-28 14:40:36 -0500

    agregadas rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  
+  get 'mental_health', to: 'pages#mental_health', as: :mental_health
+  get 'about_us', to: 'pages#about_us', as: :about_us
+
   resources :subforums do
     resources :posts, only: %i[show new create edit update destroy] do
       resources :comments, only: %i[create destroy]
     end
-  get 'videocall', to: 'calls#videocall'
-
   end
   resources :calls
   resources :journals

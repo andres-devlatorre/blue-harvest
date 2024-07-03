ed5f88bde551736f5d844b8b84a4172bb916d7a8
Andres delaTorre <andresdevlatorre@gmail.com>
2024-07-03 09:57:01 -0500

    listo para test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV[“www.talkblueharvest.me”] || “localhost:3000” }
+    end
 end

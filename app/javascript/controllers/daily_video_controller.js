44ab3ec04510f3e92388df27d60aa26e5b180f30
Andres delaTorre <andresdevlatorre@gmail.com>
2024-07-04 17:37:33 -0500

    cambios varios al final del día

--- app/javascript/controllers/daily_video_controller.js
+++ app/javascript/controllers/daily_video_controller.js
@@ -1,31 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
-
 // Connects to data-controller="daily-video"
 export default class extends Controller {
   static targets = ['daily']
-
   connect() {
   }
-
   endCall() {
     this.call.leave();
     this.call.destroy();
     this.call = null;
   }
-
   joinCall() {
     this.call = window.Daily.createFrame({
       iframeStyle: {
-        position: 'right',
+        position: 'fixed',
         border: '1px solid black',
         mode: 'grid',
-        width: '1000px',
-        height: '450px',
-        right: '1em',
-        bottom: '1em',
+        width: '50%',
+        height: '500px',
+        right: '0px',
+        top: '250px',
       },
     });
     this.call.join({ url: 'https://blue-harvest.daily.co/blue-harvest-public' });
-
   }
 }

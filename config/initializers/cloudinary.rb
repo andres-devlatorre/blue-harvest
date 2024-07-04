d910dac4ad217683516c9b556b6c22d793d6a563
Andres delaTorre <andresdevlatorre@gmail.com>
2024-07-04 09:36:31 -0500

    arreglado cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dadnbkjfv'
+  config.api_key = '543359832164198'
+  config.api_secret = 'IXvFCr2brFs6EvE1Sv6-DRt-V-g'
+  config.secure = true
+end
